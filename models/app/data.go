82093094e3af4f2fa3f47176030b8744f5fb4f68
hzy576625322 <576625322@qq.com>
2015-08-12 23:23:03 +0800

    update

--- models/app/data.go
+++ models/app/data.go
@@ -1,24 +1,46 @@
 package app
 
 import (
+	"fmt"
 	"time"
 
 	"github.com/ascoders/as"
 )
 
 type App struct {
-	Path    string `json:"path" sql:"varchar(10)" gorm:"primary_key" valid:"required;minLength(2);maxLength(10)"` // 英文路径
-	Name    string `json:"name" sql:"char(10);unique" valid:"required;minLength(1);maxLength(10)"`       // 讨论组名称
-	Manager int    `json:"manager"`                                                                            // 管理员id
+	as.Data
+
+	// 英文路径 注册后无法修改
+	Path string `json:"path" sql:"type:varchar(10);unique_index" valid:"required;minLength(2);maxLength(10)"`
+
+	// app名称
+	Name string `json:"name" sql:"type:char(10);unique" valid:"required;minLength(1);maxLength(10)"`
+
+	// 管理员id 关联后无法修改
+	Manager int `json:"manager" sql:"type:int unsigned"` // 管理员id
+
 	// Managers  []string      `json:"managers"`             // 版主id列表（协助版主只能操作帖子，不能进管理台）
-	Type    string    `json:"type" sql:"char(10)"` // 分类
-	Logo    string    `json:"logo" sql:"char(50)"` // logo地址
-	Icon    string    `json:"icon" sql:"char(50)"` // icon地址
-	Hot     int       `json:"hot" sql:"tinyint"`   // 活跃度
-	Created time.Time `json:"created"`             // 成立时间
+
+	// 所属分类
+	Type string `json:"type" sql:"type:char(10)"`
+
+	// logo地址
+	Logo string `json:"logo" sql:"type:char(100)"`
+
+	// icon地址
+	Icon string `json:"icon" sql:"type:char(100)"`
+
+	// 活跃度 0~255 * 2
+	Hot int `json:"hot" sql:"type:tinyint unsigned"`
+
+	// 创建时间
+	Created time.Time `json:"created" sql:"type:timestamp"`
 }
 
 func init() {
 	// 初始化表
 	as.Db.AutoMigrate(&App{})
+
+	value := as.Db.Where(map[string]interface{}{}).Count(&App{}).Value
+	fmt.Println(value)
 }
