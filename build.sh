c2d6432ad24c9efca9862630033d8146da652450
黄子毅 <576625322@qq.com>
2015-08-20 13:30:20 +0800

    move fis-conf

---
+++ build.sh
@@ -0,0 +1,13 @@
+echo 'start download golang dependency packages..'
+go get -u github.com/ascoders/woku
+
+echo 'start install fis3..'
+npm install -g fis3
+
+echo 'start npm install..'
+npm install jshint
+npm install fis-parser-babelcore
+npm install fis-parser-less
+
+echo 'run fis3 dev mode..'
+fis3 release -d static -r src -f fis-conf.js -w -l
