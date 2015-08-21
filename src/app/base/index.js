30d82a16ca686c781e7330a3379f05999f9a0813
黄子毅 <576625322@qq.com>
2015-08-21 18:51:07 +0800

    update

--- src/app/base/index.js
+++ src/app/base/index.js
@@ -2,7 +2,7 @@ ctrl.$onEnter = function (param, rs, rj) {
     // 隐藏公共菜单
     avalon.vmodels.global.menu.show = false
 
-    // 获取基础数据
+    // 获取基础数 据
     wk.get({
         url: '/api/apps/' + param.path,
         success: function (data) {
