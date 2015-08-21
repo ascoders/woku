25a8d616014042de1487cb11d0a1e92d4fe0f979
黄子毅 <576625322@qq.com>
2015-08-21 19:54:05 +0800

    使用atom格式化html代码 调试登录功能

--- src/auth/register/vm.js
+++ src/auth/register/vm.js
@@ -1,48 +1,48 @@
 var vm = avalon.define({
-	// 步骤
-	steps: {
-		info: {
-			name: '快速填写信息',
-			icon: 'browser',
-			locked: false
-		},
-		email: {
-			name: '邮箱',
-			icon: 'mail',
-			locked: true
-		},
-		success: {
-			name: '成功',
-			icon: 'checkmark',
-			locked: true
-		}
-	},
-	// 步骤名
-	step: 'info',
-	// 修改步骤
-	setStep: function (name) {
-		vm.step = name
-	},
+    // 步骤
+    steps: {
+        info: {
+            name: '快速填写信息',
+            icon: 'browser',
+            locked: false
+        },
+        email: {
+            name: '邮箱',
+            icon: 'mail',
+            locked: true
+        },
+        success: {
+            name: '成功',
+            icon: 'checkmark',
+            locked: true
+        }
+    },
+    // 步骤名
+    step: 'info',
+    // 修改步骤
+    setStep: function (name) {
+        vm.step = name
+    },
 
-	// 刷新验证码
-	freshCap: function () {
-		vm.data.captcha = ''
+    // 刷新验证码
+    freshCap: function () {
+        vm.data.captcha = ''
 
-		// 刷新验证码
-		wk.post({
-			url: '/api/captcha',
-			success: function (data) {
-				vm.data.capid = data.captchaCode
-			}
-		})
-	},
+        // 刷新验证码
+        wk.post({
+            url: '/api/captcha',
+            success: function (data) {
+                vm.data.capid = data.captchaCode
+            }
+        })
+    },
 
-	// 注册表单数据
-	data: {
-		email: '',
-		nickname: '',
-		password: '',
-		capid: '',
-		captcha: ''
-	}
+    // 注册表单数据
+    data: {
+        email: '',
+        nickname: '',
+        password: '',
+        capid: '',
+        captcha: ''
+    }
 })
